d6004cb67c36e8aa2252cd6a46473927d0846572
Amar <amar2115425@akgec.ac.in>
2024-05-26 09:16:50 +0530

    updated

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,6 +5,9 @@ import {
   forgotPasswordController,
   testController,
   updateProfileController,
+  getOrdersController,
+  getAllOrdersController,
+  orderStatusController,
 } from "../controllers/authController.js";
 
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
@@ -38,4 +41,17 @@ router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
 //update profile
 router.put("/profile", requireSignIn, updateProfileController);
 
+//orders
+router.get("/orders", requireSignIn, getOrdersController);
+
+//all orders
+router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
+
+// order status update
+router.put(
+  "/order-status/:orderId",
+  requireSignIn,
+  isAdmin,
+  orderStatusController
+);
 export default router;
